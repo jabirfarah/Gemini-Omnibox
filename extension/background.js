ad6044d03fa3213261b6a610eb88f7e150568fe0
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2024-01-16 21:17:19 +0000

    add new logo

--- extension/background.js
+++ extension/background.js
@@ -14,14 +14,20 @@ chrome.omnibox.onInputEntered.addListener(async function (text) {
     if (tabId == tab.id && changeInfo.status == "complete") {
       // The tab has finished loading, remove the listener and send the message
       chrome.tabs.onUpdated.removeListener(listener);
+      //Adds a 2 second buffer in case of too many attempts
+      setTimeout(
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              action: "PROMPT",
+              text: text,
+            });
+          }
+        ),
 
-      // Send a message to the content script to interact with the page
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: "PROMPT",
-          text: text,
-        });
-      });
+        2000
+      );
     }
   });
 });
