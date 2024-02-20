a4f8cbf55ba02d99a25f6173679ec877f7f3dc5e
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2024-02-19 23:03:27 -0500

    initial commit

--- extension/background.js
+++ extension/background.js
@@ -8,37 +8,40 @@ function openNewTab(url) {
 
 chrome.omnibox.onInputEntered.addListener(async function (text) {
   console.log("Input entered: " + text);
-  let url = "https://chat.openai.com";
+  let url = "https://gemini.google.com/app";
   openNewTab(url);
 
   // Wait for the new tab to finish loading
-  chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
-    if (tabId == tab.id && changeInfo.status == "complete") {
-      // The tab has finished loading, remove the listener and send the message
-      chrome.tabs.onUpdated.removeListener(listener);
-      console.log(tabId);
-      console.log(tab)
-      console.log(changeInfo)
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: "PROMPT",
-          text: text,
+  chrome.tabs.onUpdated.addListener(function listener(tabId, completion, tab) {
+    setTimeout(() => {
+      if (tabId == tab.id && completion.status == "complete") {
+        // The tab has finished loading, remove the listener and send the message
+        chrome.tabs.onUpdated.removeListener(listener);
+        console.log(tabId);
+        console.log(tab);
+        console.log(completion);
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+          chrome.tabs.sendMessage(tab.id, {
+            action: "PROMPT",
+            text: text,
+          });
         });
-      });
-    }
+      }
+    }, 3000)
+    
   });
 });
 
 chrome.contextMenus.create({
   id: "myContextMenu",
-  title: "Use selected text as ChatGPT prompt",
+  title: "Use selected text as Gemini prompt",
   contexts: ["selection"]
 })
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   console.log(info)
   console.log(tab)
-  openNewTab("https://chat.openai.com");
+  openNewTab("https://gemini.google.com/app");
 
   chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, tab) {
     if (tabId == tab.id && changeInfo.status == "complete") {
--- extension/content.js
+++ extension/content.js
@@ -4,20 +4,22 @@ var __webpack_exports__ = {};
   !*** ./src/index.js ***!
   \**********************/
 // Listen for a message from the background script to start interacting with the page
+console.log("file read!");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("code running!");
   if (request.action == "PROMPT") {
     // Your code to interact with the page, e.g., input text into a textarea
-    var input = document.querySelector("#prompt-textarea");
-    console.log(input);
-    if (input) {
-      input.value = request.text;
-      // Trigger the input event to ensure any event listeners on the textarea are notified of the change
-      input.dispatchEvent(new Event("input", {
-        bubbles: true
-      }));
-      var button = document.querySelector("#prompt-textarea~button");
-      button.click();
-    }
+    var input = document.querySelector("#app-root > side-navigation-v2 > bard-sidenav-container > bard-sidenav-content > div > main > div.content-container.ng-tns-c1097586666-0 > chat-window > div.chat-container.ng-tns-c1234278934-2.ui-v2-enabled.ng-star-inserted.at-least-desktop-small > div.bottom-container.ng-tns-c1234278934-2.ui-v2-enabled.ng-star-inserted > div.input-area-container.ng-tns-c1234278934-2.ng-star-inserted > input-area-v2 > div > div > div.text-input-field_textarea-wrapper.ng-tns-c3265487382-4.with-expansion-padding > div > div > rich-textarea > div.ql-editor.ui-v2-enabled.textarea > p");
+    console.log(input.value);
+    console.log("if statement ran!!");
+    input.dispatchEvent(new Event("input", {
+      bubbles: true
+    }));
+    input.value = request.text;
+    // Trigger the input event to ensure any event listeners on the textarea are notified of the change
+    // const button = document.querySelector("#prompt-textarea~button");
+    // button.click();
+    console.log("timeout complete!");
   }
 });
 /******/ })()
